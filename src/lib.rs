7535b2bba889bda9d969c66a1519c9e4db71649d
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2023-02-07 15:18:56 +0900

    grib2ファイルに"GRIB"が記録されていることを確認する機能を実装

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
 use std::collections::HashMap;
+use std::io::{BufReader, Read};
+use std::str;
+
+use anyhow::anyhow;
 use time::OffsetDateTime;
 
 pub struct GRIB2Info {
@@ -24,4 +28,61 @@ pub struct GRIB2Info {
     pub maxv: u16,
     /// レベル値と物理値(mm/h)の対応を格納するコレクション
     pub level_values: HashMap<u16, u16>,
+}
+
+impl GRIB2Info {
+    fn dummy_default() -> Self {
+        Self {
+            date_time: OffsetDateTime::now_utc(),
+            number_of_points: 0,
+            top: 47995833,
+            left: 118006250,
+            bottom: 20004167,
+            right: 149993750,
+            h_increment: 12500,
+            v_increment: 8333,
+            data_per_bits: 8,
+            maxv: 98,
+            level_values: HashMap::new(),
+        }
+    }
+}
+
+/// grib2ファイルの先頭から4倍と読み込んで、"GRIB"が記録されているか確認する。
+fn read_grib<R: Read>(reader: &mut BufReader<R>) -> anyhow::Result<()> {
+    let mut buf = [0; 4];
+
+    let length = reader.read(&mut buf)?;
+    if length != 4 {
+        return Err(anyhow!("can not read `GRIB` from the file"));
+    }
+    let s = str::from_utf8(buf.as_slice())?;
+    match s {
+        "GRIB" => Ok(()),
+        _ => Err(anyhow!("can not read `GRIB` from the file")),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs::File;
+
+    use super::*;
+
+    const SAMPLE_FILE: &'static str = "fixtures/20200707000000_grib2.bin";
+
+    /// grib2ファイルを開いて、GRIBを読み込めるか確認する。
+    #[test]
+    fn can_read_grib() {
+        let mut reader = BufReader::new(File::open(SAMPLE_FILE).unwrap());
+        assert!(read_grib(&mut reader).is_ok());
+    }
+
+    /// grib2ファイル以外から、GRIBを読み込めないことを確認する。
+    #[test]
+    fn can_not_read_grib() {
+        let dummy = "XXXX";
+        let mut reader = BufReader::new(dummy.as_bytes());
+        assert!(read_grib(&mut reader).is_err());
+    }
 }
