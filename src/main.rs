1427c42daee318cfed4aeb5695a6198025e6bec1
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2023-02-09 19:32:22 +0900

    ヘルプを日本語で表示

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ use grib2csv::{BoundaryBuilder, Grib2Csv};
     name = "grib2csv",
     version = "0.0.1",
     author = "xjr1300.04@gmail.com",
-    about = "GRIB2通報式による1kmメッシュ解析雨量または降水短時間予報データを、CSV形式のファイルに変換します。"
+    about = "GRIB2通報式による1kmメッシュ解析雨量または降水短時間予報データを、CSV形式のファイルに変換します。\n\n\
+             格子点を出力する領域を指定する場合、度単位の緯度または経度を1,000,000倍した値を指定してください。"
 )]
 struct Args {
     /// 入力GRIB2ファイル
@@ -15,35 +16,19 @@ struct Args {
     input: String,
 
     /// CSVファイルに出力する格子点の最北端の緯度
-    #[arg(
-        short,
-        long,
-        help = "latitude of the northernmost to be output(ex. 36532213)"
-    )]
+    #[arg(short, long, help = "格子点を出力する最北端の緯度(例:36000000)")]
     northernmost: Option<u32>,
 
     /// CSVファイルに出力する格子点の最南端の緯度
-    #[arg(
-        short,
-        long,
-        help = "latitude of the southernmost to be output(ex. 35432213)"
-    )]
+    #[arg(short, long, help = "格子点を出力する最南端の緯度(例:35000000)")]
     southernmost: Option<u32>,
 
     /// CSVファイルに出力する格子点の最西端の経度
-    #[arg(
-        short,
-        long,
-        help = "longitude of the westernmost to be output(ex. 135532213)"
-    )]
+    #[arg(short, long, help = "格子点を出力する最西端の経度(例:135000000)")]
     westernmost: Option<u32>,
 
     /// CSVファイルに出力する格子点の最西端の経度
-    #[arg(
-        short,
-        long,
-        help = "longitude of the westernmost to be output(ex. 136532213)"
-    )]
+    #[arg(short, long, help = "格子点を出力する最東端の経度(例:136000000)")]
     easternmost: Option<u32>,
 
     /// 出力CSVファイル
